2a9b1d12a26624e7654883e7efb81ed3309442f8
AlenaShkr <elalshkr@gmail.com>
2020-02-17 22:58:52 +0300

    youtube-client/feat: add view data to result card

--- src/app/components/result-card/result-card.component.ts
+++ src/app/components/result-card/result-card.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ResultCard } from '../../models/result-card.model';
 
 @Component({
   selector: 'app-result-card',
   templateUrl: './result-card.component.html',
-  styleUrls: ['./result-card.component.scss']
+  styleUrls: ['./result-card.component.scss'],
 })
+
 export class ResultCardComponent implements OnInit {
   public resultCard: ResultCard;
-
+  @Input() public card: ResultCard;
   constructor() { };
 
   public ngOnInit(): void {
